ab46fdb62ef1367f609551c3c497bf1157a4fade
Vladi <39379713+vladik93@users.noreply.github.com>
2023-08-09 00:12:40 +0300

    created a toggleable sidebar

---
+++ script.js
@@ -0,0 +1,11 @@
+function openSideNav() {
+  let sideNav = document.querySelector(".header-side-nav");
+
+  sideNav.classList.add("open");
+}
+
+function closeSideNav() {
+  let sideNav = document.querySelector(".header-side-nav");
+
+  sideNav.classList.remove("open");
+}
