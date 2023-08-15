c618695cce3f1ff8049efaa82363be8c221b9e62
Vladi <39379713+vladik93@users.noreply.github.com>
2023-08-15 22:32:20 +0300

    Creating javascript part for slideshow

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Side Nav
+
 function openSideNav() {
   let sideNav = document.querySelector(".header-side-nav");
 
@@ -9,3 +11,35 @@ function closeSideNav() {
 
   sideNav.classList.remove("open");
 }
+
+// Testimonial Slides
+let slideIndex = 0;
+
+let btn = document.getElementById("testimonial-right-btn");
+let testimonialItems = document.querySelectorAll(".testimonial-item");
+let testimonialsContainer = document.getElementById("testimonials-container");
+let indicatorsContainer = document.querySelector(".indicators");
+
+btn.addEventListener("click", function () {
+  slideIndex++;
+  if (slideIndex < testimonialItems.length) {
+    testimonialsContainer.style = `transform: translateX(${
+      slideIndex * -100
+    }%)`;
+  } else {
+    slideIndex = 0;
+
+    testimonialsContainer.style = `transform: translateX(${
+      slideIndex * -100
+    }%)`;
+  }
+});
+
+testimonialItems.forEach((item, index) => {
+  let span = document.createElement("span");
+  indicatorsContainer.appendChild(span);
+
+  if (index === slideIndex) {
+    span.style.backgroundColor = "red";
+  }
+});
